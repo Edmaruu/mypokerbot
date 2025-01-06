83f09494dc41e3a3959fbb50da89f2ec9557aee8
Edmaruu <mamboedwinpau.inchaud@epfedu.fr>
2025-01-06 14:35:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pypokerengine
+from pypokerengine.api.game import setup_config, start_poker
+from fish_player import FishPlayer
+from console_player import ConsolePlayer
+from random_player import RandomPlayer
+
+config = setup_config(max_round=10, initial_stack=100, small_blind_amount=20)
+config.register_player(name="f1", algorithm=FishPlayer())
+config.register_player(name="f2", algorithm=FishPlayer())
+config.register_player(name="f3", algorithm=FishPlayer())
+config.register_player(name="f4", algorithm=FishPlayer())
+config.register_player(name="f5", algorithm=RandomPlayer())
+config.register_player(name="r1", algorithm=RandomPlayer())
+config.register_player(name="r2", algorithm=RandomPlayer())
+config.register_player(name="r3", algorithm=RandomPlayer())
+config.register_player(name="r4", algorithm=RandomPlayer())
+config.register_player(name="r5", algorithm=RandomPlayer())
+game_result = start_poker(config, verbose=1)
